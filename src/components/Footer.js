62c9de101a80cd7ce2a1bfe44747ffdffe3bf826
marup <marupatlavimalraj@gmail.com>
2025-03-05 02:50:50 +0530

    footer fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,19 @@
-import React from react;
+import React from "react";
 
-const Footer = () = {
+const Footer = () => {
   return (
-    footer style={styles.footer}
-      p© 2024 My Portfolio. All rights reserved.p
-    footer
+    <footer style={styles.footer}>
+      <p>© 2024 My Portfolio. All rights reserved.</p>
+    </footer>
   );
 };
 
 const styles = {
-  footer {
-    textAlign center,
-    padding 20px,
-    background #333,
-    color white,
+  footer: {
+    textAlign: "center",
+    padding: "20px",
+    background: "#333",
+    color: "white",
   },
 };
 
